a1b412f39262da6e3282dcd0fdf5b1f5499593ae
Jorge Solis <jsoli@sintonyze.com>
2020-09-24 19:34:04 +0200

    First commit

---
+++ src/build/precache-manifest.9366dd3a728d03811aa7e3b49b0c24e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fd58577ca891a5d798676fc66640df3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bf1eb96ff4fa5babd91",
+    "url": "/static/css/main.18e6dac5.chunk.css"
+  },
+  {
+    "revision": "f23e57b3d241ada3648c",
+    "url": "/static/js/2.b0c3dd10.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b0c3dd10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bf1eb96ff4fa5babd91",
+    "url": "/static/js/main.2fe43dd4.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
